bc82a13dc9d854bf4e96604b2094270b4bdb2a93
Biszku <dbichu@onet.pl>
2023-09-13 00:21:21 +0200

    imported basic styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "../styles/base/_reset.scss";
+import "../styles/base/_typography.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
