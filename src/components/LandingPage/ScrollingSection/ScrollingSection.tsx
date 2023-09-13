630782777686c108d17cf3daae1e9689de726276
Biszku <dbichu@onet.pl>
2023-09-13 20:55:20 +0200

    implemented useScroll and rendered sections

---
+++ src/components/LandingPage/ScrollingSection/ScrollingSection.tsx
@@ -0,0 +1,22 @@
+"use client";
+import styles from "./ScrollingSection.module.scss";
+import { useRef } from "react";
+import { useScroll } from "framer-motion";
+import Welcome from "./Sections/Welcome/Welcome";
+import AboutMe from "./Sections/AboutMe/AboutMe";
+import Projects from "./Sections/Projects/Projects";
+import Skills from "./Sections/Skills/Skills";
+
+const ScrollingSection = () => {
+  const SectionsContainer = useRef(null);
+  const { scrollYProgress } = useScroll({ target: SectionsContainer });
+  return (
+    <section ref={SectionsContainer} className={styles.container}>
+      {[Welcome, AboutMe, Projects, Skills].map((Component, index) => (
+        <Component key={index} scrollProgress={scrollYProgress} />
+      ))}
+    </section>
+  );
+};
+
+export default ScrollingSection;
