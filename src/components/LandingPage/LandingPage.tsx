8e095b48e9703f18bbbea8ace53f84de5bf025cc
Biszku <dbichu@onet.pl>
2023-09-13 20:53:12 +0200

    imported two Sections

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,7 +1,12 @@
+import ContactSection from "./ContactSection/ContactSection";
+import ScrollingSection from "./ScrollingSection/ScrollingSection";
+import styles from "./LandingPage.module.scss";
+
 const LandingPage = () => {
   return (
-    <main>
-      <p>Hello World!</p>
+    <main className={styles.container}>
+      <ScrollingSection />
+      <ContactSection />
     </main>
   );
 };
