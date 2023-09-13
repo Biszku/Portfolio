69f3720ba4ef952b2a094c49d81270d248f3974b
Biszku <dbichu@onet.pl>
2023-09-13 20:54:22 +0200

    created contact section pattern

---
+++ src/components/LandingPage/ContactSection/ContactSection.tsx
@@ -0,0 +1,11 @@
+import styles from "./ContactSection.module.scss";
+
+const ContactSection = () => {
+  return (
+    <section className={styles.container}>
+      <p>Contact Section</p>
+    </section>
+  );
+};
+
+export default ContactSection;
