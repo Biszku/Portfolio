2613c8096a2db35e28de3102be0ee7b65b443b9d
Biszku <dbichu@onet.pl>
2023-09-13 20:49:04 +0200

    removed typography import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/base/_reset.scss";
-import "../styles/base/_typography.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
