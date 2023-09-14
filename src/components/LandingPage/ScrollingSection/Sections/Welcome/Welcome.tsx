4854fba98b8a7535efa4c53ed082ce4202cc7f7d
Biszku <dbichu@onet.pl>
2023-09-14 21:13:20 +0200

    added navigation and header

--- src/components/LandingPage/ScrollingSection/Sections/Welcome/Welcome.tsx
+++ src/components/LandingPage/ScrollingSection/Sections/Welcome/Welcome.tsx
@@ -9,8 +9,18 @@ const Welcome = ({
   scrollProgress: MotionValue<number>;
 }) => {
   const [isDisplayed, setIsDisplayed] = useState(true);
+  const [textElementVisibility, setTextElementVisibility] = useState(false);
+  const [isVisibleNavigation, setIsVisibleNavigation] = useState(false);
 
   useEffect(() => {
+    const textElementTimer = setTimeout(
+      () => setTextElementVisibility(true),
+      625
+    );
+    const navigationTimer = setTimeout(
+      () => setIsVisibleNavigation(true),
+      1000
+    );
     const handleScroll = () => {
       const scrollY = window.scrollY;
       const windowHeight = window.innerHeight * 7;
@@ -27,24 +37,68 @@ const Welcome = ({
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      clearTimeout(textElementTimer);
+      clearTimeout(navigationTimer);
     };
   }, []);
 
   return (
     <AnimatePresence>
       {isDisplayed && (
-        <motion.article
+        <motion.div
+          initial={{ opacity: 0, y: -200 }}
+          animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, x: -600 }}
           className={styles.container}
         >
-          <Image
-            src="/avatar.jpg"
-            width={300}
-            height={300}
-            alt="Biszku's avatar"
-            className={styles.container_image}
-          />
-        </motion.article>
+          <motion.article layout className={styles.container_avatar}>
+            <Image
+              src="/avatar.jpg"
+              width={300}
+              height={300}
+              alt="Biszku's avatar"
+              className={styles.container_avatar_avatar}
+            />
+          </motion.article>
+
+          {textElementVisibility && (
+            <motion.article
+              initial={{ opacity: 0, x: 400 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ type: "tween" }}
+              className={styles.text_Content}
+            >
+              <motion.header layout className={styles.text_Content_HeaderBox}>
+                <h1 className={styles.text_Content_HeaderBox_Header}>
+                  Welcome to my website
+                </h1>
+              </motion.header>
+              {isVisibleNavigation && (
+                <motion.div
+                  transition={{ type: "spring", duration: 1, stiffness: 50 }}
+                  initial={{ opacity: 0, x: 400 }}
+                  animate={{ opacity: 1, x: 0 }}
+                >
+                  <ul className={styles.navigationBox}>
+                    {["About me", "Projects", "Skills", "Contact"].map(
+                      (el, index) => (
+                        <motion.li
+                          key={index}
+                          style={{
+                            fontSize: "3rem",
+                          }}
+                          className={styles.navigationBox_item}
+                        >
+                          {el}
+                        </motion.li>
+                      )
+                    )}
+                  </ul>
+                </motion.div>
+              )}
+            </motion.article>
+          )}
+        </motion.div>
       )}
     </AnimatePresence>
   );
