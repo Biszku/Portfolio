8a71f3311bf67c75a006b3b59d10b78f0086d29b
Biszku <dbichu@onet.pl>
2023-09-16 20:41:15 +0200

    created logic for ScrollToTop visibility

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,35 @@
+"use client";
 import ContactSection from "./ContactSection/ContactSection";
 import ScrollingSection from "./ScrollingSection/ScrollingSection";
 import styles from "./LandingPage.module.scss";
+import ScrollToTop from "./ScrollToTop/ScrollToTop";
+import { useState, useEffect } from "react";
 
 const LandingPage = () => {
+  const [isScrollingRocketVisible, setIsScrollingRocketVisible] =
+    useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      if (scrollY >= window.innerHeight * 2) {
+        setIsScrollingRocketVisible(true);
+      } else {
+        setIsScrollingRocketVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <main className={styles.container}>
       <ScrollingSection />
       <ContactSection />
+      <ScrollToTop rocketVisibility={isScrollingRocketVisible} />
     </main>
   );
 };
