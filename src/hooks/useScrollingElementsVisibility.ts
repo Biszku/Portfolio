2671e971e383d126b208603a06988a1fc0b663e0
Biszku <dbichu@onet.pl>
2023-09-17 23:15:14 +0200

    created hook to easly managing states

---
+++ src/hooks/useScrollingElementsVisibility.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const useScrollingElementsVisibility = (min: number, max: number) => {
+  const [isDisplayed, setIsDisplayed] = useState(true);
+  const [textElementVisibility, setTextElementVisibility] = useState(false);
+  const [isVisibleNavigation, setIsVisibleNavigation] = useState(false);
+
+  useEffect(() => {
+    const textElementTimer = setTimeout(
+      () => setTextElementVisibility(true),
+      625
+    );
+    const navigationTimer = setTimeout(
+      () => setIsVisibleNavigation(true),
+      1000
+    );
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight * 8;
+      const scrollProgress = scrollY / windowHeight;
+
+      if (scrollProgress >= min && scrollProgress < max) {
+        setIsDisplayed(true);
+      } else {
+        setIsDisplayed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearTimeout(textElementTimer);
+      clearTimeout(navigationTimer);
+      setIsDisplayed(false);
+    };
+  }, []);
+
+  return [isDisplayed, textElementVisibility, isVisibleNavigation];
+};
+
+export default useScrollingElementsVisibility;
