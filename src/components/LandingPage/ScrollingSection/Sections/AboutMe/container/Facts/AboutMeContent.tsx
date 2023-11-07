37c2819f8c02ba9d0c037ac7b50a4f38c989558c
Biszku <dbichu@onet.pl>
2023-11-07 15:23:44 +0100

    done about me content

--- src/components/LandingPage/ScrollingSection/Sections/AboutMe/container/Facts/AboutMeContent.tsx
+++ src/components/LandingPage/ScrollingSection/Sections/AboutMe/container/Facts/AboutMeContent.tsx
@@ -1,7 +1,18 @@
+import styles from "./AboutMeContent.module.scss";
+import Image from "next/image";
+import List from "./AboutMeList/List";
+
 const AboutMeContent = () => {
   return (
-    <div>
-      <h1>AboutMeContent</h1>
+    <div className={styles.content}>
+      <Image
+        src="/avatar.jpg"
+        width={250}
+        height={250}
+        alt="My picture"
+        className={styles.image}
+      />
+      <List />
     </div>
   );
 };
