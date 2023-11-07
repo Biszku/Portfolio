b88dd78172150cc806f8d73a929a2923e60c00c3
Biszku <dbichu@onet.pl>
2023-11-07 15:23:55 +0100

    done list to about me section

---
+++ src/components/LandingPage/ScrollingSection/Sections/AboutMe/container/Facts/AboutMeList/List.tsx
@@ -0,0 +1,26 @@
+import styles from "./List.module.scss";
+
+const List = () => {
+  return (
+    <ol className={styles.list}>
+      <li>
+        <span>Name</span>: Daniel
+      </li>
+      <li>
+        <span>Age</span>: 19
+      </li>
+      <li>
+        <span>Gender</span>: Programmer
+      </li>
+      <li>
+        <span>Country</span>: Poland
+      </li>
+      <li>
+        <span>Description</span>: I am have been learning programming for a 1
+        year. I am friedly and calm person.
+      </li>
+    </ol>
+  );
+};
+
+export default List;
