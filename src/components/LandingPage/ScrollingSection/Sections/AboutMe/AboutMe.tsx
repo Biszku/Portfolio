4d45f8d255ede81a793eaef0e4a24e9aabc8e905
Biszku <dbichu@onet.pl>
2023-11-07 13:34:14 +0100

    code reduction

--- src/components/LandingPage/ScrollingSection/Sections/AboutMe/AboutMe.tsx
+++ src/components/LandingPage/ScrollingSection/Sections/AboutMe/AboutMe.tsx
@@ -1,32 +1,9 @@
-import { MotionValue, motion, AnimatePresence } from "framer-motion";
-import styles from "./AboutMe.module.scss";
 import useScrollingElementsVisibility from "@/src/hooks/useScrollingElementsVisibility";
+import AboutMeSection from "./container/AboutMeContainer";
 
-const AboutMe = ({
-  scrollProgress,
-}: {
-  scrollProgress: MotionValue<number>;
-}) => {
+const AboutMe = () => {
   const isDisplayed = useScrollingElementsVisibility(0.25, 0.5);
-
-  return (
-    <AnimatePresence>
-      {isDisplayed && (
-        <motion.article
-          initial={{ opacity: 0, x: 600 }}
-          animate={{
-            opacity: 1,
-            x: 0,
-            transition: { delay: 0.2 },
-          }}
-          exit={{ opacity: 0, x: -600 }}
-          className={styles.container}
-        >
-          <p>About Me</p>
-        </motion.article>
-      )}
-    </AnimatePresence>
-  );
+  return <AboutMeSection display={isDisplayed} />;
 };
 
 export default AboutMe;
