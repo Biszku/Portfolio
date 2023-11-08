2f0fdcb7d15b2b00e0c07138f5aa5bc7b436b9a5
Biszku <dbichu@onet.pl>
2023-11-08 18:37:07 +0100

    Done Projects Section Container

--- src/components/LandingPage/ScrollingSection/Sections/Projects/Projects.tsx
+++ src/components/LandingPage/ScrollingSection/Sections/Projects/Projects.tsx
@@ -1,10 +1,56 @@
 import { MotionValue, motion, AnimatePresence } from "framer-motion";
-
 import styles from "./Projects.module.scss";
 import useScrollingElementsVisibility from "@/src/hooks/useScrollingElementsVisibility";
+import Card from "./Card/Card";
 
-const Projects = ({scrollProgress}:{scrollProgress: MotionValue<number>}) => {
+const Projects = ({
+  scrollProgress,
+}: {
+  scrollProgress: MotionValue<number>;
+}) => {
   const isDisplayed = useScrollingElementsVisibility(0.5, 0.75);
+  const projectsInfo = [
+    {
+      name: "Events Website",
+      image: "/events.PNG",
+      resolution: {
+        height: 991,
+        width: 1904,
+      },
+      alt: "Events screenshot",
+      description: "A site to buy tickets to various events at the best price",
+      technologies: ["React.js", "Sass"],
+      linkToLiveVersion: "https://biszku-project.vercel.app/",
+      linkToGithub: "https://github.com/Biszku/Events-page-project",
+    },
+    {
+      name: "Quiz App",
+      image: "/quizio.PNG",
+      resolution: {
+        height: 811,
+        width: 469,
+      },
+      alt: "Quiz App screenshot",
+      description:
+        "Quiz application to test your knowledge in various side of IT",
+      technologies: ["Next.js", "Sass", "CSS Modules"],
+      linkToLiveVersion: "https://quiz-io.vercel.app/",
+      linkToGithub: "https://github.com/Biszku/QuizIo",
+    },
+    {
+      name: "Chess",
+      image: "/chess.PNG",
+      resolution: {
+        height: 991,
+        width: 1920,
+      },
+      alt: "Chess App screenshot",
+      description: "Just chess :)",
+      technologies: ["Vanilla TypeScript"],
+      linkToLiveVersion: "https://biszku-chess-game.vercel.app/",
+      linkToGithub: "https://github.com/Biszku/vanilla-ts-chess-game",
+    },
+  ];
 
   return (
     <AnimatePresence>
@@ -15,7 +61,9 @@ const Projects = ({scrollProgress}:{scrollProgress: MotionValue<number>}) => {
           exit={{ opacity: 0, x: -600 }}
           className={styles.container}
         >
-          <p>Projects</p>
+          {projectsInfo.map((project, index) => (
+            <Card key={index} info={project} />
+          ))}
         </motion.article>
       )}
     </AnimatePresence>
