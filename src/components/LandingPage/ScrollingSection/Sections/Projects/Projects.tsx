a50dd9a655d26b38a10029d3ec456e19e18491c8
Biszku <dbichu@onet.pl>
2023-11-09 21:06:49 +0100

    added icons to list

--- src/components/LandingPage/ScrollingSection/Sections/Projects/Projects.tsx
+++ src/components/LandingPage/ScrollingSection/Sections/Projects/Projects.tsx
@@ -2,6 +2,13 @@ import { MotionValue, motion, AnimatePresence } from "framer-motion";
 import styles from "./Projects.module.scss";
 import useScrollingElementsVisibility from "@/src/hooks/useScrollingElementsVisibility";
 import Card from "./Card/Card";
+import {
+  IoLogoCss3,
+  IoLogoSass,
+  IoLogoReact,
+  IoLogoVercel,
+  IoLogoJavascript,
+} from "react-icons/io5";
 
 const Projects = ({
   scrollProgress,
@@ -14,12 +21,15 @@ const Projects = ({
       name: "Events Website",
       image: "/events.PNG",
       resolution: {
-        height: 991,
-        width: 1904,
+        height: (991 * 2) / 3,
+        width: (1904 * 2) / 3,
       },
       alt: "Events screenshot",
       description: "A site to buy tickets to various events at the best price",
-      technologies: ["React.js", "Sass"],
+      technologies: [
+        { name: "React.js", icon: <IoLogoReact /> },
+        { name: "Sass", icon: <IoLogoSass /> },
+      ],
       linkToLiveVersion: "https://biszku-project.vercel.app/",
       linkToGithub: "https://github.com/Biszku/Events-page-project",
     },
@@ -27,13 +37,17 @@ const Projects = ({
       name: "Quiz App",
       image: "/quizio.PNG",
       resolution: {
-        height: 811,
-        width: 469,
+        height: 811 * 2,
+        width: 469 * 2,
       },
       alt: "Quiz App screenshot",
       description:
         "Quiz application to test your knowledge in various side of IT",
-      technologies: ["Next.js", "Sass", "CSS Modules"],
+      technologies: [
+        { name: "Next.js", icon: <IoLogoVercel /> },
+        { name: "Sass", icon: <IoLogoSass /> },
+        { name: "CSS Modules", icon: <IoLogoCss3 /> },
+      ],
       linkToLiveVersion: "https://quiz-io.vercel.app/",
       linkToGithub: "https://github.com/Biszku/QuizIo",
     },
@@ -41,12 +55,14 @@ const Projects = ({
       name: "Chess",
       image: "/chess.PNG",
       resolution: {
-        height: 991,
-        width: 1920,
+        height: (991 * 2) / 3,
+        width: (1920 * 2) / 3,
       },
       alt: "Chess App screenshot",
       description: "Just chess :)",
-      technologies: ["Vanilla TypeScript"],
+      technologies: [
+        { name: "Vanilla TypeScript", icon: <IoLogoJavascript /> },
+      ],
       linkToLiveVersion: "https://biszku-chess-game.vercel.app/",
       linkToGithub: "https://github.com/Biszku/vanilla-ts-chess-game",
     },
