5ccc290ae8b8b97728a0fa58144ba80dd4cd9ce8
Biszku <dbichu@onet.pl>
2023-11-15 21:19:22 +0100

    exported icons

---
+++ src/components/svgs/index.ts
@@ -0,0 +1,5 @@
+export { default as TypescriptIcon } from "./typescript.svg";
+export { default as SassIcon } from "./Sass.svg";
+export { default as ReactJsIcon } from "./ReactJs.svg";
+export { default as NextJsIcon } from "./NextJs.svg";
+export { default as CssIcon } from "./Css.svg";
