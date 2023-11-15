a5c28c81cd1cd911376113173a690cec23b073ed
Biszku <dbichu@onet.pl>
2023-11-15 21:19:05 +0100

    implemented svgr

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
