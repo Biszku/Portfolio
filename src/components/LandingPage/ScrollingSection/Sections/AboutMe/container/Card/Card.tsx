a5637cbc9f6a75aad9b3e38263913c7bd03713ea
Biszku <dbichu@onet.pl>
2023-11-21 15:28:15 +0100

    Card redesign

--- src/components/LandingPage/ScrollingSection/Sections/AboutMe/container/Card/Card.tsx
+++ src/components/LandingPage/ScrollingSection/Sections/AboutMe/container/Card/Card.tsx
@@ -1,23 +1,12 @@
 import styles from "./Card.module.scss";
 
-const Card = ({
-  header,
-
-  list,
-}: {
-  header: string;
-
-  list: string[];
-}) => {
+const Card = ({ list }: { list: string[] }) => {
   return (
-    <div className={styles.content}>
-      <p className={styles.header}>{header}</p>
-      <ol className={styles.list}>
-        {list.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ol>
-    </div>
+    <ol className={styles.list}>
+      {list.map((name, index) => (
+        <li key={index}>{name}</li>
+      ))}
+    </ol>
   );
 };
 
